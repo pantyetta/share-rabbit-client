672f8f70b8dc0034bdd553b4fb7671cc091cda0f
pantyetta <email@pantyetta.com>
2023-09-30 23:59:50 +0900

    all delete button at mainpage

--- chrome extension/src/view/mainPage.vue
+++ chrome extension/src/view/mainPage.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="flex flex-col gap-2">
-    <PostCard
-      v-for="data in history.history"
-      :key="data.id"
-      :post="data"
-    ></PostCard>
+  <div>
+    <button
+      class="btn btn-sm mb-2 ml-auto flex"
+      @click="history.$reset()"
+      v-show="history.history.length > 0"
+    >
+      <span>Clear ALl</span
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
+    <div class="flex flex-col gap-2">
+      <PostCard
+        v-for="data in history.history"
+        :key="data.id"
+        :post="data"
+      ></PostCard>
+    </div>
   </div>
 </template>
 
